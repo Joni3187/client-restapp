4211cdfca53cc6e8808e875e11cfcd37c3c02626
Joni3187 <oni.rene@outlook.com>
2021-11-12 22:56:39 -0600

    modify config file

--- next.config.js
+++ next.config.js
@@ -4,10 +4,7 @@ module.exports = {
         domains: ["res.cloudinary.com"],
     },
     env: {
-        "BASE_URL": process.env.BASE_URL,
-        "DATABASE_URI": "mongodb+srv://Admin:ipartS187%40%21@cluster0.8yr08.mongodb.net/restapp-final?retryWrites=true&w=majority",
-        "MONGODB_URL": "mongodb+srv://Admin:ipartS187%40%21@cluster0.8yr08.mongodb.net/restapp-final?retryWrites=true&w=majority",
         "NEXT_PUBLIC_API_URL": process.env.NEXT_PUBLIC_API_URL,
-        "NEXT_PUBLIC_DATABASE_URL": "postgres://strapi:strapi@localhost:5432/strapi?synchroniz:rue"
+        "NEXT_PUBLIC_DATABASE_URL": process.env.NEXT_PUBLIC_DATABASE_URL
     }
 }
